89569d6feca1dc2f70fed3a0f17b5ed6328a4041
Souvik Misra <souvik.misra@maersk.com>
2021-07-31 12:16:02 +0530

    Added skills - initial commit

--- src/views/MySkills.js
+++ src/views/MySkills.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import './MySkills.css';
 
 function MySkills(props) {
+    const skills = [
+        {
+            src: '/html-5.png',
+            text: 'HTML5'
+        },
+        {
+            src: '/css.png',
+            text: 'CSS3'
+        },
+        {
+            src: '/javascript.png',
+            text: 'JavaScript'
+        },
+        {
+            src: '/vue-js.png',
+            text: 'Vue JS'
+        },
+        {
+            src: '/nodejs.png',
+            text: 'Node JS'
+        },
+        {
+            src: '/logo192.png',
+            text: 'React JS'
+        },
+        {
+            src: '/command-window.png',
+            text: 'Terminal'
+        },
+        {
+            src: '/python.png',
+            text: 'Python'
+        }
+    ];
     return (
-        <div>
-            Bro
+        <div className="my-skills-container">
+            {
+                skills.map(skill => (
+                    <div className="skill-container">
+                        <img className="skill-img" src={skill.src} alt={skill.text} />
+                        <p className="skill-text">{skill.text}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
