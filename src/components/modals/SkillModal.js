d76194e024738abd4143a606cc365a8f85c315eb
Souvik Misra <souvik.misra@maersk.com>
2021-07-31 14:41:36 +0530

    Added skills - Made modals more descriptive

--- src/components/modals/SkillModal.js
+++ src/components/modals/SkillModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Avatar from '../Avatar';
 import './SkillModal.css';
 
 function SkillModal(props) {
@@ -6,7 +7,11 @@ function SkillModal(props) {
         <div className="skill-modal" style={{ display: props.show ? 'block':'none'}}>
             <div className="skill-modal-content">
                 <span className="close" onClick={() => props.toggle(false)}>&times;</span>
-                <p>{props.content}</p>
+                <h2>{props.content.text}</h2>
+                <div className="skill-modal-info">
+                    <Avatar alt={props.content.text} src={props.content.src} />
+                    <p>{props.content.desc}</p>
+                </div>
             </div>
         </div>
     );
--- src/views/MySkills.js
+++ src/views/MySkills.js
@@ -6,51 +6,64 @@ function MySkills(props) {
     const skills = [
         {
             src: '/html-5.png',
-            text: 'HTML5'
+            text: 'HTML5',
+            desc: 'Skilled for 8+ years. Important for setting up the structure of web application.'
         },
         {
             src: '/css.png',
-            text: 'CSS3'
+            text: 'CSS3',
+            desc: 'Skilled for 8+ years. Important for styling the web pages with proper design standards.'
         },
         {
             src: '/javascript.png',
-            text: 'JavaScript'
+            text: 'JavaScript',
+            desc: 'Skilled for 8+ years. Required for adding behaviour and logic to the web application.'
         },
         {
             src: '/vue-js.png',
-            text: 'Vue JS'
+            text: 'Vue JS',
+            desc: 'Skilled for 2 years. Framework for developing logical and seamless SPAs with separation of controls.'
         },
         {
             src: '/nodejs.png',
-            text: 'Node JS'
+            text: 'Node JS',
+            desc: 'Skilled for 5+ years. Essential for web application development and installing dependencies.'
         },
         {
             src: '/logo192.png',
-            text: 'React JS'
+            text: 'React JS',
+            desc: 'Skilled for 3+ years. Framework for easy and lucid development of SPAs with readable structure and logic. Also the most widely used UI framework.'
         },
         {
             src: '/github.png',
-            text: 'Git'
+            text: 'Git',
+            desc: 'Skilled for 6+ years. One of the most widely used open source version control management systems.'
         },
         {
             src: '/python.png',
-            text: 'Python'
+            text: 'Python',
+            desc: 'Skilled for 1+ years. Essential for back-end development and data sciences'
         }
     ];
     const [showModal, toggleModal] = useState(false);
+    const [index, setIndex] = useState(0);
+    function handleClickOnSkill(id){
+        setIndex(id);
+        toggleModal(!showModal);
+    }
     return (
         <>
             <div className="my-skills-container">
                 {
-                    skills.map(skill => (
-                        <div className="skill-container" onClick={() => toggleModal(!showModal)}>
+                    skills.map((skill, id) => (
+                        <div className="skill-container" onClick={handleClickOnSkill.bind(null,id)}>
                             <img className="skill-img" src={skill.src} alt={skill.text} />
                             <p className="skill-text">{skill.text}</p>
                         </div>
                     ))
                 }
             </div>
-            <SkillModal show={ showModal } toggle={toggleModal} />
+            <SkillModal show={ showModal } toggle={toggleModal} content={skills[index]} />
         </>
     );
 }
