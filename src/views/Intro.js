399546742c70bb1cd0d4a3bc33c40e3ed11f3d4d
Souvik Misra <souvik.misra@maersk.com>
2021-07-31 11:33:55 +0530

    Added Material icons

--- src/views/Intro.js
+++ src/views/Intro.js
@@ -10,23 +10,27 @@ function Intro(props) {
             <div className="personal-info-container">
                 <p className="name">Souvik Misra</p>
                 <p className="desig">Software Engineer - Senior Frontend</p>
-                <ul>
-                    <li>
+                <ul className="info-list">
+                    <li className="info-list-item">
+                        <span class="material-icons icon">home</span>
                         <span className="info">
                             <p className="data">Bengaluru, Karnataka, IN</p>
                         </span>
                     </li>
-                    <li>
+                    <li className="info-list-item">
+                        <span class="material-icons icon">email</span>
                         <span className="info">
                             <p classname="data">svkmsr6@gmail.com</p>
                         </span>
                     </li>
-                    <li>
+                    <li className="info-list-item">
+                        <span class="material-icons icon">phone</span>
                         <span className="info">
                             <p className="data">+918884677455</p>
                         </span>
                     </li>
-                    <li>
+                    <li className="info-list-item">
+                        <span class="material-icons icon">travel_explore</span>
                         <span className="info">
                             <p className="data">
                                 I am also on <a className="data" href="https://github.com/svkmsr6">GitHub</a> and <a className="data" href="https://www.linkedin.com/in/souvik-misra-0a281929/">LinkedIn</a>!
